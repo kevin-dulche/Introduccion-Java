c16aacdd5e2a35ec66af579029065091ae39fcb0
Kevin Dulche Laptop GMR Windows <kevdulche@gmail.com>
2023-09-06 14:54:35 -0600

    Alcance de las variables y sentencia ELSE

--- Codigos/IfStatement.java
+++ Codigos/IfStatement.java
@@ -7,6 +7,15 @@ public class IfStatement {
             // Send file
             fileSended++;
             System.out.println("Archivo Enviado");
+            int i = 0;
+            i++;
+        } else {
+            fileSended--;
+            System.out.println("Por favor enciende tu bluetooth, para iniciar la transferencia");
         }
+
+        System.out.println(isBluetoothEnabled);
+        System.out.println(fileSended);
+        // System.out.println(i); <- Esto no es posible
     }
 }
