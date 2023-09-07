3ed18c1d24af45267c698f3970e30ba087fafc27
Kevin Dulche Laptop GMR Windows <kevdulche@gmail.com>
2023-09-06 21:27:08 -0600

    Funciones en Java

---
+++ Codigos/Funciones.java
@@ -0,0 +1,14 @@
+public class Funciones {
+    /*
+     * Estrutura de las funciones
+    mod.acceso tipoDeValorDeRegreso nombreDeFuncion(Argumentos/Parametros){
+        return valorRegreso;
+    }
+
+    void no regresa ningun dato
+
+    Llamar a funcion
+
+    tipoDato nombreVariable = nombreDeFuncion(Argumentos/Parametros)
+     */
+}
