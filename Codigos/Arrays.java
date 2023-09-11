83af98730bcf47cad020c317e2768a3f0c3e653d
Kevin Dulche PC Windows <kevdulche@gmail.com>
2023-09-10 19:14:17 -0600

    Declaracion de Arrays

---
+++ Codigos/Arrays.java
@@ -0,0 +1,22 @@
+public class Arrays {
+    public static void main(String[] args) {
+        String[] andoridVersions = new String[17];
+        String days[] = new String[7];
+
+        String[][] cities = new String[4][2]; // 4 * 2 = 8
+
+        /*
+        * +------------------------------+
+        * |  Country    |  City          |
+        * --------------------------------
+        * | México      | CDMX           |
+        * | México      | Guadalajara    |
+        * | Colombia    | Bogotá         |
+        * | Colombia    | Medellín       |
+        * +------------------------------+
+        * */
+
+        int[][][] numbers = new int[2][2][2];
+        int[][][][] numbers4 = new int[2][2][2][2];
+    }
+}
